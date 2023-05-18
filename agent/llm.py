4c3f4f46ae07042c7252e258a5f9e562957f80e7
ZeroZY-bgp <736530911@qq.com>
2023-05-18 09:50:10 +0800

    重构代码，修改提示词结构

--- agent/llm.py
+++ agent/llm.py
@@ -1,147 +1,38 @@
-import copy
-import os
-import re
-
 import openai
 from abc import abstractmethod
 
 import agent.useless as ul
 from transformers import AutoTokenizer, AutoModel
 
-from agent.utils import append_to_str_file, create_txt, load_txt_to_str, load_last_n_lines
-
-DEBUG_MODE = True
-
 
 class BaseLLM:
     max_token: int = 10000
     temperature: float = 0.01
     top_p = 0.9
-    history = []
-    basic_history = []
-    history_len: int = 10
-    basic_token_len = 0
-    total_token_size = 0
-    max_history_size = 600
-    window_decrease_size = 400
     model_name = ""
-    ai_name = ""
-    user_name = ""
-    lock_memory = False
-
-    def __init__(self, info, user_name, lock_memory, history_window):
-        self.info = info
-        self.ai_name = self.info.ai_name
-        self.user_name = user_name
-        self.lock_memory = lock_memory
-        self.history_window = history_window
 
     @property
     def _llm_type(self) -> str:
         return self.model_name
 
-    def get_history(self):
-        return self.history
-
-    def load_history(self, basic_history):
-        self.basic_history = basic_history
-        self.basic_token_len = len(self.basic_history[0][0]) + len(self.basic_history[0][1])
-        self.total_token_size = 0
-        if os.path.exists(self.info.history_path) and os.path.getsize(self.info.history_path) == 0:
-            # 历史记录为空
-            self.history = copy.deepcopy(self.basic_history)
-        else:
-            self.history = copy.deepcopy(self.basic_history)
-            # 加载历史记录最后几行
-            history_lst = load_last_n_lines(self.info.history_path, self.history_window)
-            pattern = r'(.+?) ' + self.info.ai_name + '说：(.+?)(?=$)'
-
-            for dialog in history_lst:
-                matches = re.findall(pattern, dialog)
-                self.history.append(matches[0])
-
-    def set_max_history_size(self, max_history_size):
-        self.max_history_size = max_history_size
-
     def chat(self,
              query: str,
-             context: str) -> str:
-        # 将上下文加入到最开头的提示词中
-        context = context.replace("{{{AI_NAME}}}", self.ai_name)
-        history_and_context = self.history[0][0].replace("{{{context}}}", context).replace("{{{AI_NAME}}}",
-                                                                                           self.ai_name).\
-            replace("{{{USER_NAME}}}", self.user_name if self.user_name != '' else '一个人')
-        first_ans = self.history[0][1].replace("{{{AI_NAME}}}", self.ai_name)
-
-        if DEBUG_MODE:
-            print("context长度:", len(context))
-            print("提示词总长度:", len(history_and_context))
-
-        self.history[0] = (history_and_context, first_ans)
-
-        if DEBUG_MODE:
-            print("记忆检索片段：")
-            print(context)
-        if self.user_name != '':
-            q = self.user_name + "说：" + query + '\n' + self.ai_name + '说：'
-        else:
-            q = query + '\n' + self.ai_name + '说：'
-        ans = self.get_response(q)
-
-        # if DEBUG_MODE and self.model_name == 'gpt-3.5-turbo':
-        #
-        #     res = openai.Moderation.create(
-        #         input=ans
-        #     )
-        #     print("flag:", res["results"][0])
-
-        self.history.append((query, ans))
-        self.total_token_size += (len(ans) + len(query))
-
-        print("Token size:", self.total_token_size + len(context))
-
-        # 恢复最开头的提示词
-        self.history[0] = self.basic_history[0]
-
-        if not self.lock_memory:
-            # 保存历史到文件中
-            append_str = q.replace('\n', ' ') + ans + '\n'
-            append_to_str_file(self.info.history_path, append_str)
-        # 窗口控制
-        self.history_window_control(context)
-        return ans
+             history: list) -> str:
+        return self.get_response(query, history)
 
     @abstractmethod
-    def get_response(self, query):
+    def get_response(self, query, history):
         return " "
-
-    def history_window_control(self, context):
-        if self.total_token_size + len(context) >= self.max_history_size:
-            while self.total_token_size + len(context) > (self.max_history_size - self.window_decrease_size):
-                try:
-                    self.total_token_size -= (len(self.history[1][0]) + len(self.history[1][1]))
-                    self.history.pop(1)
-                except IndexError:
-                    # print("窗口不能再缩小了")
-                    break
-            if DEBUG_MODE:
-                print("窗口缩小， 历史对话：")
-                print(self.history)
 
 
 class Gpt3_5LLM(BaseLLM):
-    model_name = 'gpt-3.5-turbo'
+    model_name = 'gpt-3.5-turbo-0301'
     temperature = 0.1
 
     def __init__(self,
-                 info,
-                 user_name,
-                 lock_memory=False,
-                 history_window=6,
                  temperature=0.1,
                  max_token=1000,
                  window_decrease_size=300):
-        super().__init__(info, user_name, lock_memory, history_window)
         self.temperature = temperature
         self.max_token = max_token
         self.window_decrease_size = window_decrease_size
@@ -154,18 +45,19 @@ class Gpt3_5LLM(BaseLLM):
             max_tokens=self.max_token
         )
 
-    def create_massages(self, query):
+    @staticmethod
+    def create_massages(query, history):
         massages = []
-        for i in range(len(self.history)):
-            massages.append({'role': 'user', 'content': self.history[i][0]})
-            massages.append({'role': 'assistant', 'content': self.history[i][1]})
+        for i in range(len(history)):
+            massages.append({'role': 'user', 'content': history[i][0]})
+            massages.append({'role': 'assistant', 'content': history[i][1]})
 
         massages.append({'role': 'user', 'content': query})
 
         return massages
 
-    def get_response(self, query):
-        massages = self.create_massages(query)
+    def get_response(self, query, history):
+        massages = self.create_massages(query, history)
         response = self.send(massages)
         return response.choices[0].message.content
 
@@ -175,14 +67,9 @@ class Gpt3_5freeLLM(BaseLLM):
     temperature = 0.1
 
     def __init__(self,
-                 info,
-                 user_name,
-                 lock_memory=False,
-                 history_window=6,
                  temperature=0.1,
                  max_token=1000,
                  window_decrease_size=200):
-        super().__init__(info, user_name, lock_memory, history_window)
         self.temperature = temperature
         self.max_token = max_token
         self.message_id = ""
@@ -195,8 +82,8 @@ class Gpt3_5freeLLM(BaseLLM):
                                     parentMessageId=self.message_id,
                                     temperature=self.temperature)
 
-    def get_response(self, query):
-        res = self.send(query, self.history[0][0])
+    def get_response(self, query, history):
+        res = self.send(query, history[0][0])
         self.talk_times += 1
         print("talk_times:", self.talk_times)
         self.message_id = res["id"]
@@ -208,15 +95,14 @@ class ChatGLMLLM(BaseLLM):
     model: object = None
     model_name = 'ChatGlm'
 
-    def __init__(self, info, user_name, temperature=0.1, lock_memory=False, history_window=6):
-        super().__init__(info, user_name, lock_memory, history_window)
+    def __init__(self, temperature=0.1):
         self.temperature = temperature
 
-    def get_response(self, query):
+    def get_response(self, query, history):
         ans, _ = self.model.chat(
             self.tokenizer,
             query,
-            history=self.history,
+            history=history,
             max_length=self.max_token,
             temperature=self.temperature,
         )
--- agent/main_agent.py
+++ agent/main_agent.py
@@ -1,18 +1,24 @@
+import copy
+import os
 import re
+
+import textdistance
 from langchain import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 
 from agent.abstract_agent import AbstractAgent
 from agent.audio import AudioModule
-from agent.utils import init_knowledge_vector_store, load_txt_to_lst, CharacterInfo, delete_last_line
+from agent.utils import init_knowledge_vector_store, load_txt_to_lst, CharacterInfo, delete_last_line, \
+    load_last_n_lines, append_to_str_file
 from agent.llm import Gpt3_5LLM, ChatGLMLLM, Gpt3_5freeLLM
 from world_manager import CharacterInfo
 from command import Pool, command_flags
 
-# embed_model_path = 'text2vec/GanymedeNil_text2vec-large-chinese'
-embed_model_path = 'text2vec/shibing624_text2vec_base_chinese'
+embed_model_path = 'text2vec/GanymedeNil_text2vec-large-chinese'
+# embed_model_path = 'text2vec/shibing624_text2vec_base_chinese'
 # embed_model_path = 'GanymedeNil/text2vec-large-chinese'
 device = 'cuda'
+DEBUG_MODE = True
 
 
 def get_docs_with_score(docs_with_score):
@@ -24,14 +30,7 @@ def get_docs_with_score(docs_with_score):
 
 
 def collect_context(text_lst):
-    return "\n".join([text.page_content for text in text_lst])
-
-
-def generate_prompt(context: str,
-                    query: str,
-                    prompt_template) -> str:
-    prompt = prompt_template.replace("{query}", query).replace("{context}", context)
-    return prompt
+    return "\n".join([text for text in text_lst])
 
 
 class VectorStore:
@@ -40,11 +39,12 @@ class VectorStore:
         self.top_k = top_k
         self.path = path
         self.core, _ = init_knowledge_vector_store(embeddings=embeddings, filepath=self.path)
-        # self.core.chunk_size = chunk_size
+        self.chunk_size = chunk_size
         # FAISS.similarity_search_with_score_by_vector = similarity_search_with_score_by_vector
 
     def similarity_search_with_score(self, query):
         if self.core is not None:
+            self.core.chunk_size = self.chunk_size
             return self.core.similarity_search_with_score(query, self.top_k)
         else:
             return []
@@ -58,6 +58,13 @@ def get_related_text_lst(query, vs, lst):
     lst.extend(get_docs_with_score(related_text_with_score))
 
 
+def mem_to_lst(mem):
+    lst = []
+    for i in range(len(mem)):
+        lst.append(mem[i].page_content)
+    return lst
+
+
 class MainAgent(AbstractAgent):
 
     def __init__(self,
@@ -67,6 +74,7 @@ class MainAgent(AbstractAgent):
                  model_name='gpt3_5',
                  lock_memory=False,
                  history_window=3,
+                 window_decrease_size=400,
                  classifier_enabled=False,
                  max_history_size=1100,
                  context_chunk_size=20,
@@ -97,6 +105,8 @@ class MainAgent(AbstractAgent):
         self.memory_search_top_k = memory_search_top_k
         self.context_chunk_size = context_chunk_size
         self.lock_memory = lock_memory
+        self.history_window = history_window
+        self.window_decrease_size = window_decrease_size
         self.query = ''  # 提问的临时存储，用于重试提问
         if speak_rate == '快':
             rate = 200
@@ -114,15 +124,17 @@ class MainAgent(AbstractAgent):
         self.embeddings = HuggingFaceEmbeddings(model_name=embedding_model_path,
                                                 model_kwargs={'device': embedding_device})
 
-        self.identity_top_k = 3
-        self.history_top_k = 12
+        self.identity_top_k = 2
+        self.history_top_k = 9
         self.event_top_k = 3
-        self.identity_vs = VectorStore(self.embeddings, self.info.identity_path, chunk_size=1,
+        self.semantic_similarity_threshold = 20.0  # 语义相似度分数阈值（相似度太小的会被去掉）
+        self.word_similarity_threshold = 0.6  # 字词相似度分数阈值（相似度太大的会被去掉）
+        self.identity_vs = VectorStore(self.embeddings, self.info.identity_path, chunk_size=30,
                                        top_k=self.identity_top_k)
         if self.lock_memory:
-            self.history_vs = VectorStore(self.embeddings, self.info.history_path, chunk_size=1,
+            self.history_vs = VectorStore(self.embeddings, self.info.history_path, chunk_size=30,
                                           top_k=self.history_top_k)
-        self.event_vs = VectorStore(self.embeddings, self.info.event_path, chunk_size=1,
+        self.event_vs = VectorStore(self.embeddings, self.info.event_path, chunk_size=30,
                                     top_k=self.event_top_k)
         print("【---记忆模块加载完成---】")
         # ---model
@@ -130,33 +142,26 @@ class MainAgent(AbstractAgent):
         if self.model_name == 'chatglm-6b-int4':
             self.path = 'THUDM/chatglm-6b-int4'
             # self.path = 'chatglm-6b-int4'
-            self.llm = ChatGLMLLM(self.info,
-                                  user_name=self.user_name,
-                                  lock_memory=self.lock_memory,
-                                  history_window=history_window,
-                                  temperature=temperature)
+            self.llm = ChatGLMLLM(temperature=temperature)
             self.llm.load_model(self.path)
         elif self.model_name == 'gpt3_5':
-            self.llm = Gpt3_5LLM(self.info,
-                                 user_name=self.user_name,
-                                 lock_memory=self.lock_memory,
-                                 history_window=history_window,
-                                 temperature=temperature)
+            self.llm = Gpt3_5LLM(temperature=temperature)
         elif model_name == 'gpt3_5free':
-            self.llm = Gpt3_5freeLLM(self.info,
-                                     user_name=self.user_name,
-                                     lock_memory=self.lock_memory,
-                                     history_window=history_window,
-                                     temperature=temperature)
+            self.llm = Gpt3_5freeLLM(temperature=temperature)
         else:
             raise AttributeError("模型选择参数出错！传入的参数为", self.model_name)
+        # 历史对话列表
+        self.history = []
         # 初始化提示语
         self.basic_history = load_txt_to_lst(self.info.prompt_path)
         # 加载短期对话历史
-        self.llm.load_history(self.basic_history)
+        # self.llm.load_history(self.basic_history)
+        self.load_history(self.basic_history)
         # 窗口控制
-        self.llm.set_max_history_size(max_history_size)
-        # ---
+        # self.llm.set_max_history_size(max_history_size)
+        self.basic_token_len = 0
+        self.total_token_size = 0
+        self.max_history_size = max_history_size
         print("【---对话模型加载完成---】")
         # ---voice
         self.voice_module = AudioModule(sound_library='local', rate=rate)
@@ -169,6 +174,7 @@ class MainAgent(AbstractAgent):
         #     print("【---话题分类器加载完成---】")
 
     def chat(self, query):
+        # ------指令部分
         self.check_command()
         var_dict = {'info': self.info}
         Pool().execute(query, var_dict)
@@ -191,28 +197,43 @@ class MainAgent(AbstractAgent):
                 # 删除历史文件最后一行
                 delete_last_line(self.info.history_path)
                 # 重新加载临时历史对话
-                self.llm.load_history(self.basic_history)
+                self.load_history(self.basic_history)
+        # ------
 
-        # 相似文本列表
-        related_text_lst = []
-        # 直接加载记忆模块
-        # 角色自己的本地记忆
-        get_related_text_lst(query, self.identity_vs, related_text_lst)
-        # 对话记忆
-        if self.lock_memory:
-            get_related_text_lst(query, self.history_vs, related_text_lst)
+        # ------文本检索部分
+        # 检索记忆（实体、对话、事件）
+        entity_lst, dialog_lst, event_lst = self.get_related_text(query)
+        # 嵌入提示词
+        entity_text = collect_context(entity_lst)
+        dialog_text = collect_context(dialog_lst)
+        event_text = collect_context(event_lst)
+        context_len = self.embedding_context(entity_text, dialog_text, event_text)
+        # ------
+
+        # 文本中加入提问者身份
+        if self.user_name != '':
+            q = self.user_name + "说：" + query + '\n' + self.ai_name + '说：'
         else:
-            get_related_text_lst(query,
-                                 VectorStore(self.embeddings, self.info.history_path,
-                                             chunk_size=10,
-                                             top_k=self.history_top_k),
-                                 related_text_lst)
-        # 事件记忆
-        get_related_text_lst(query, self.event_vs, related_text_lst)
+            q = query + '\n' + self.ai_name + '说：'
 
-        context = collect_context(related_text_lst)
+        # ---与大模型通信
+        ans = self.llm.chat(q, self.history)
+        # ---
 
-        ans = self.llm.chat(query, context)
+        self.history.append((query, ans))
+        self.total_token_size += (len(ans) + len(query))
+
+        print("Token size:", self.total_token_size + context_len)
+
+        # 恢复最开头的提示词
+        self.history[0] = self.basic_history[0]
+
+        if not self.lock_memory:
+            # 保存历史到文件中
+            append_str = q.replace('\n', ' ') + ans + '\n'
+            append_to_str_file(self.info.history_path, append_str)
+        # 窗口控制
+        self.history_window_control(context_len)
 
         # if self.classifier_enabled:
         #     topic_tag = self.classifier.do(ans)
@@ -224,6 +245,111 @@ class MainAgent(AbstractAgent):
         self.query = query
         return ans
 
+    def history_window_control(self, context_len):
+        if self.total_token_size + context_len >= self.max_history_size:
+            while self.total_token_size + context_len > (self.max_history_size - self.window_decrease_size):
+                try:
+                    self.total_token_size -= (len(self.history[1][0]) + len(self.history[1][1]))
+                    self.history.pop(1)
+                except IndexError:
+                    # print("窗口不能再缩小了")
+                    break
+            if DEBUG_MODE:
+                print("窗口缩小， 历史对话：")
+                print(self.history)
+
+    def load_history(self, basic_history):
+        self.basic_history = basic_history
+        self.basic_token_len = len(self.basic_history[0][0]) + len(self.basic_history[0][1])
+        self.total_token_size = 0
+        if os.path.exists(self.info.history_path) and os.path.getsize(self.info.history_path) == 0:
+            # 历史记录为空
+            self.history = copy.deepcopy(self.basic_history)
+        else:
+            self.history = copy.deepcopy(self.basic_history)
+            # 加载历史记录最后几行
+            history_lst = load_last_n_lines(self.info.history_path, self.history_window)
+            pattern = r'(.+?) ' + self.info.ai_name + '说：(.+?)(?=$)'
+
+            for dialog in history_lst:
+                matches = re.findall(pattern, dialog)
+                self.history.append(matches[0])
+
+    def embedding_context(self, entity, dialog, event):
+
+        entity = entity.replace("{{{AI_NAME}}}", self.ai_name)
+        dialog = dialog.replace("{{{AI_NAME}}}", self.ai_name)
+        event = event.replace("{{{AI_NAME}}}", self.ai_name)
+
+        context = self.history[0][0]
+        context = context.replace("{{{ENTITY}}}", entity)
+        context = context.replace("{{{DIALOG}}}", dialog)
+        context = context.replace("{{{EVENT}}}", event)
+        context = context.replace("{{{AI_NAME}}}", self.ai_name)
+        context = context.replace("{{{USER_NAME}}}", self.user_name)
+
+        first_ans = self.history[0][1].replace("{{{AI_NAME}}}", self.ai_name)
+
+        context_len = len(entity) + len(dialog) + len(event)
+        if DEBUG_MODE:
+            print("context长度:", context_len)
+            print("提示词总长度:", len(context))
+
+        self.history[0] = (context, first_ans)
+        print(self.history[0])
+
+        if DEBUG_MODE:
+            print("实体记忆：")
+            print(entity)
+            print("对话记忆：")
+            print(dialog)
+            print("事件记忆：")
+            print(event)
+        return context_len
+
+    def get_related_text(self, query):
+        # 实体记忆
+        entity_mem = []
+        get_related_text_lst(query, self.identity_vs, entity_mem)
+        entity_lst = mem_to_lst(entity_mem)
+        # 字词高相似度去重
+        entity_lst = self.high_similarity_text_filter(entity_lst)
+
+        # 对话记忆
+        dialog_mem = []
+        if self.lock_memory:
+            get_related_text_lst(query, self.history_vs, dialog_mem)
+        else:
+            get_related_text_lst(query,
+                                 VectorStore(self.embeddings, self.info.history_path,
+                                             chunk_size=10,
+                                             top_k=self.history_top_k),
+                                 dialog_mem)
+        dialog_lst = mem_to_lst(dialog_mem)
+        dialog_lst = self.high_similarity_text_filter(dialog_lst)
+
+        # 事件记忆
+        event_mem = []
+        get_related_text_lst(query, self.event_vs, event_mem)
+        event_lst = mem_to_lst(event_mem)
+        event_lst = self.high_similarity_text_filter(event_lst)
+
+        return entity_lst, dialog_lst, event_lst
+
+    def high_similarity_text_filter(self, text_lst):
+        # 字词相似度比对，算法复杂度o(n^2)
+        remaining_strings = list(text_lst)  # 创建一个副本以避免在迭代时修改原始列表
+        for i in range(len(text_lst)):
+            for j in range(len(text_lst)):
+                if i != j:
+                    sim_score = textdistance.jaccard(text_lst[i], text_lst[j])
+                    if sim_score > self.word_similarity_threshold:
+                        # 如果两个字符串的相似度超过阈值，则删除较短的字符串（较短的字符串信息含量大概率较少）
+                        del_str = text_lst[i] if len(text_lst[i]) < len(text_lst[j]) else text_lst[j]
+                        if del_str in remaining_strings:
+                            remaining_strings.remove(del_str)
+        return remaining_strings
+
     def check_command(self):
         if command_flags.ai_name != self.ai_name:
             return
@@ -233,17 +359,17 @@ class MainAgent(AbstractAgent):
                 self.history_vs = VectorStore(self.embeddings, self.info.history_path, chunk_size=1,
                                               top_k=self.history_top_k)
             # 重新加载临时历史对话
-            self.llm.load_history(self.basic_history)
+            self.load_history(self.basic_history)
         elif command_flags.prompt:
             # 提示词被打开过，重新加载提示词和历史对话
             self.basic_history = load_txt_to_lst(self.info.prompt_path)
-            self.llm.load_history(self.basic_history)
+            self.load_history(self.basic_history)
         command_flags.reset()
 
     def check_show_command(self):
         if command_flags.show_temp_history:
             # 展示当前临时历史窗口
-            window = self.llm.get_history()
+            window = self.history
             for dialog in window[1:]:
                 print(dialog[0])
                 print(self.ai_name + '说：' + dialog[1])
