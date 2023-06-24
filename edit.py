e01eae787df7474ae3746112f2bc660633b647c5
ZeroZY-bgp <736530911@qq.com>
2023-06-24 21:28:55 +0800

    原main函数只保留编辑世界功能

--- edit.py
+++ edit.py
@@ -1,70 +1,61 @@
 import sys
-
-import openai
-import configparser
 import os
 
 from tools.utils import CharacterInfo
 from template import PROMPT_TEMPLATE, IDENTITY_TEMPLATE
-from world_manager import Manager
+from worldmanager import Manager
 from worldsandbox import Sandbox
+from config import BaseConfig
 
 
 if __name__ == '__main__':
 
     print("【---欢迎使用AI chat with memory---】")
     while True:
-        print("输入数字以选择功能：\n1.与ai对话\n2.ai之间对话\n3.管理世界\n4.打开世界文件夹\n5.打开设置")
+        # print("输入数字以选择功能：\n1.与ai对话\n2.ai之间对话\n3.管理世界\n4.打开世界文件夹\n5.打开设置")
+        print("输入数字以选择功能：\n1.管理世界\n2.打开世界文件夹\n3.打开设置")
         option = input()
+        # if option == '1':
+        #     config = BaseConfig()
+        #     # 检查参数是否合法
+        #     world_name = config.world_name
+        #     ai_name = config.ai_name
+        #     manager = Manager(world_name)
+        #     if not manager.world_is_created:
+        #         print(world_name, "世界未创建，请检查config.ini文件。")
+        #         break
+        #     if not manager.character_is_created(ai_name):
+        #         print(ai_name, "人物未创建，请检查config.ini文件。")
+        #         break
+        #
+        #     print("设置完毕")
+        #     Sandbox(world_name).chat_with_one_agent(config)
+        #     sys.exit(0)
+        # elif option == '2':
+        #     config = BaseConfig()
+        #     # 检查参数是否合法
+        #     world_name = config.world_name
+        #     ai_name = config.ai_name
+        #     manager = Manager(world_name)
+        #     if not manager.world_is_created:
+        #         print(world_name, "世界未创建，请检查config.ini文件。")
+        #         break
+        #
+        #     is_created = True
+        #     for name in config.ai_names:
+        #         if not manager.character_is_created(name):
+        #             print(name, "人物未创建，请检查config.ini文件。")
+        #             is_created = False
+        #             break
+        #     if not is_created:
+        #         break
+        #     if len(config.ai_names) == 0:
+        #         print("空ai列表，请检查config.ini文件。")
+        #         break
+        #     print("设置完毕")
+        #     Sandbox(world_name).chat_with_multi_agent(config.ai_names, config)
+
         if option == '1':
-            config = configparser.ConfigParser()
-            config.read('config.ini', encoding='utf-8-sig')
-            openai.api_key = config.get('API', 'openai_api_key')
-            # 检查参数是否合法
-            world_name = config['WORLD']['name']
-            ai_name = config['AI']['name']
-            manager = Manager(world_name)
-            if not manager.world_is_created:
-                print(world_name, "世界未创建，请检查config.ini文件。")
-                break
-            if not manager.character_is_created(ai_name):
-                print(ai_name, "人物未创建，请检查config.ini文件。")
-                break
-
-            print("设置完毕")
-            Sandbox(world_name).chat_with_one_agent(config)
-            sys.exit(0)
-        elif option == '2':
-            config = configparser.ConfigParser()
-            config.read('config.ini', encoding='utf-8-sig')
-            openai.api_key = config.get('API', 'openai_api_key')
-            # 检查参数是否合法
-            world_name = config['WORLD']['name']
-            ai_names = config['MULTI_AI']['names']
-            # 获取名字列表
-            valid_punctuation = ['、', '，', '.', '。', '|', '/']
-            for p in valid_punctuation:
-                ai_names = ai_names.replace(p, ',')
-            ai_names = ai_names.split(',')
-            ai_names = [name for name in ai_names if name]
-
-            manager = Manager(world_name)
-            if not manager.world_is_created:
-                print(world_name, "世界未创建，请检查config.ini文件。")
-                break
-
-            is_created = True
-            for name in ai_names:
-                if not manager.character_is_created(name):
-                    print(name, "人物未创建，请检查config.ini文件。")
-                    is_created = False
-                    break
-            if not is_created:
-                break
-            print("设置完毕")
-            Sandbox(world_name).chat_with_multi_agent(ai_names, config)
-
-        elif option == '3':
             print("【---欢迎使用世界管理器---】")
             while True:
                 print("你想做什么？\n1.创建新世界；2.创建新人物；3.返回")
@@ -110,15 +101,15 @@ if __name__ == '__main__':
                     break
                 else:
                     print("请输入正确的数字(1-3)")
-        elif option == '4':
+        elif option == '2':
             w_name = input("输入世界名称：")
             path = os.path.abspath('memory\\' + w_name)
             try:
                 os.startfile(path)
             except FileNotFoundError:
                 print("没有该世界对应的文件夹，请确认是否拼写正确。")
-        elif option == '5':
+        elif option == '3':
             path = os.path.abspath('config.ini')
             os.startfile(path)
         else:
-            print("请输入正确数字(1-5)。")
+            print("请输入正确数字(1-3)。")
