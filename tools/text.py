ff9cef0b9f869835b77a8a0a6b38543e090d95c5
ZeroZY-bgp <736530911@qq.com>
2023-06-28 18:30:34 +0800

    删除冗余代码

--- tools/text.py
+++ tools/text.py
@@ -1,55 +1,12 @@
 import ast
 import copy
-import os
-
 import textdistance
 from langchain import FAISS
-from langchain.text_splitter import CharacterTextSplitter
 import re
 from typing import List
 
-SENTENCE_SIZE = 100
 
-
-# 代码来源:https://github.com/imClumsyPanda/langchain-ChatGLM/blame/master/textsplitter/chinese_text_splitter.py
-class ChineseTextSplitter(CharacterTextSplitter):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def split_text(self, text: str) -> List[str]:
-        text = re.sub(r'([;；.!?。！？\?])([^”’])', r"\1\n\2", text)  # 单字符断句符
-        text = re.sub(r'(\.{6})([^"’”」』])', r"\1\n\2", text)  # 英文省略号
-        text = re.sub(r'(\…{2})([^"’”」』])', r"\1\n\2", text)  # 中文省略号
-        text = re.sub(r'([;；!?。！？\?]["’”」』]{0,2})([^;；!?，。！？\?])', r'\1\n\2', text)
-        # 如果双引号前有终止符，那么双引号才是句子的终点，把分句符\n放到双引号后，注意前面的几句都小心保留了双引号
-        text = text.rstrip()  # 段尾如果有多余的\n就去掉它
-        # 很多规则中会考虑分号;，但是这里我把它忽略不计，破折号、英文双引号等同样忽略，需要的再做些简单调整即可。
-        ls = [i for i in text.split("\n") if i]
-        for ele in ls:
-            if len(ele) > SENTENCE_SIZE:
-                ele1 = re.sub(r'([,，.]["’”」』]{0,2})([^,，.])', r'\1\n\2', ele)
-                ele1_ls = ele1.split("\n")
-                for ele_ele1 in ele1_ls:
-                    if len(ele_ele1) > SENTENCE_SIZE:
-                        ele_ele2 = re.sub(r'([\n]{1,}| {2,}["’”」』]{0,2})([^\s])', r'\1\n\2', ele_ele1)
-                        ele2_ls = ele_ele2.split("\n")
-                        for ele_ele2 in ele2_ls:
-                            if len(ele_ele2) > SENTENCE_SIZE:
-                                ele_ele3 = re.sub('( ["’”」』]{0,2})([^ ])', r'\1\n\2', ele_ele2)
-                                ele2_id = ele2_ls.index(ele_ele2)
-                                ele2_ls = ele2_ls[:ele2_id] + [i for i in ele_ele3.split("\n") if i] + ele2_ls[
-                                                                                                       ele2_id + 1:]
-                        ele_id = ele1_ls.index(ele_ele1)
-                        ele1_ls = ele1_ls[:ele_id] + [i for i in ele2_ls if i] + ele1_ls[ele_id + 1:]
-
-                id = ls.index(ele)
-                ls = ls[:id] + [i for i in ele1_ls if i] + ls[id + 1:]
-        return ls
-
-
-class AnswerFragmentTextSplitter(CharacterTextSplitter):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+class AnswerFragmentTextSplitter:
 
     def split_text(self, text_lst: List[str], text_splitter) -> List[str]:
         all_text = ''
@@ -71,9 +28,7 @@ class LineSplitter:
         return words
 
 
-class SentenceSplitter(CharacterTextSplitter):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+class SentenceSplitter:
 
     def split_text(self, text) -> List[str]:
         # 中英文标点符号
